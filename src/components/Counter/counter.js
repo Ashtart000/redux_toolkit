01389073f430b2b9d7e0e38796ebbbbc25008874
Anna Lednyova <ashtart@gmail.com>
2023-10-19 19:14:21 +0300

    connect Counter

--- src/components/Counter/counter.js
+++ src/components/Counter/counter.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { increment, decrement } from '../../store/slices/counterSlice';
 
 const Counter = (props) => {
+    const { count, step, dispatch} = props;
+
     return (
         <div>
-            <p>Count: {1}</p>
-            <p>Step: {1}</p>
-            <button>Increment</button>
-            <button>Decrement</button>
+            <p>Count: {count}</p>
+            <p>Step: {step}</p>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
         </div>
     );
 }
 
-export default Counter;
+function mapStateToProps(state) {
+    return {
+        count: state.count,
+        step: state.step
+    };
+}
+
+// const withState = connect(mapStateToProps);
+
+// const counterWithState = withState(Counter);
+
+export default connect(mapStateToProps)(Counter);
